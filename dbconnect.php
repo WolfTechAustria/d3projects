a4c2a7d7c426455add48b4f189cbd8c285702c64
Wolfgang Obrist <wo@wolftech.at>
2025-03-19 19:48:27 +0100

    add auth to gitignore

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
-$servername = 'localhost';
+$servername = 'wsrv-1.wolftech.at';
 $username = 'DBAdmin';
 $password = '3v0E@bd71!!';
 $dbname = 'd3_projects';
 
 $db = mysqli_connect("$servername", "$username", "$password", "$dbname");
-if(!$db){
+if (!$db) {
   echo mysqli_connect_error();
 }
-?>
